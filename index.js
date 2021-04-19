af5a7a33aa5e98e7d4df95aacd0b6e02e5b16a3b
Andressa Soares Rodrigues <>
2021-04-19 01:57:24 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.get('/', (req, res) =>{
+    res.render('perguntar');
+    
+});
+/*app.get('/perguntar', (req, res) =>{
+    res.render('perguntar');
+    
+});*/
+
+app.listen(3000, ()=>{
+  console.log("App server is running on port 3000");
+});
+
